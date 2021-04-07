07496b52be6329eb7cf191e984e831cb7c3b325b
MaksatRysbekov <maksatrysbekov96@gmail.com>
2021-04-07 23:07:19 +0200

    add scroll

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -3,13 +3,11 @@ import 'file:///D:/flutter-projects/udemy-maximilian/expense_app/lib/model/trans
 import 'package:expense_app/widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   final titleController = TextEditingController();
   final priceController = TextEditingController();
 
@@ -24,20 +22,22 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Expense App"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text("CHART!"),
-                elevation: 5,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text("CHART!"),
+                  elevation: 5,
+                ),
               ),
-            ),
-            UserTransactions()
-          ],
+              UserTransactions()
+            ],
+          ),
         ),
       ),
     );
